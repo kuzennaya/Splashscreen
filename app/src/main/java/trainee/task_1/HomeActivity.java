c7d945da13dd4e4f07650b062eca730e4f45b5c3
kuzennaya <4ervya4-ok@mail.ru>
2015-10-27 19:35:37 +0400

    Fix 2nd test

--- app/src/main/java/trainee/task_1/HomeActivity.java
+++ app/src/main/java/trainee/task_1/HomeActivity.java
@@ -13,12 +13,17 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ListView listView = (ListView) findViewById(R.id.listView1);
+        ListView listView = (ListView) findViewById(R.id.listView);
 
         final String[] items = getResources().getStringArray(R.array.items);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
 
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
