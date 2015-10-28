157b560c9f37e52a2582efe957f68f258b4fbce8
kuzennaya <4ervya4-ok@mail.ru>
2015-10-28 14:37:37 +0400

    Dont know what to do

--- app/src/main/java/trainee/task_1/SplashActivity.java
+++ app/src/main/java/trainee/task_1/SplashActivity.java
@@ -16,30 +16,40 @@ public class SplashActivity extends Activity {
         }
 
         if (!isInitial) {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(SplashActivity.this, HomeActivity.class));
-                    SplashActivity.this.finish();
-                }
-            }, 2000);
-            isInitial = true;
+            try {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                        isInitial = true;
+                        SplashActivity.this.finish();
+                    }
+                }, 2000);
+                isInitial = true;
+            }
+            catch (Exception e) {
+                isInitial = true;
+                e.printStackTrace();
+                SplashActivity.this.finish();
+            }
         }
     }
     @Override
     public void onBackPressed() {
         isInitial = true;
-    }
+        SplashActivity.this.finish();
+        super.onBackPressed();
+  }
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
         if (savedInstanceState != null && savedInstanceState.containsKey("iniFlag")) {
             isInitial = savedInstanceState.getBoolean("iniFlag");
         }
+        super.onRestoreInstanceState(savedInstanceState);
     }
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putBoolean("iniFlag", isInitial);
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
